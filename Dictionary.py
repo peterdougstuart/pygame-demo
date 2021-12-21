12c8a6f69a3924893036d8063f43c38c3d7f8e1f
LH-Coding <92981024+LH-Coding@users.noreply.github.com>
2021-12-21 10:56:38 +0000

    Update game.py

---
+++ Dictionary.py
@@ -0,0 +1,4 @@
+FoodPrice = {"Toast":0.50,"Beans":0.20,"Eggs":0.60,"Bacon":0.80}
+FoodOrdered = []
+order = input("What do you want to eat? (Toast/Beans/Eggs/Bacon) ")
+print("£" + str(FoodPrice[order]))
