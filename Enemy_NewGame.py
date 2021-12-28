2b8510e2577dee791f8e59bcf1802fc3ddbc5e22
LH-Coding <92981024+LH-Coding@users.noreply.github.com>
2021-12-28 12:42:20 +0000

    Added Game

--- Enemy_NewGame.py
+++ Enemy_NewGame.py
@@ -1,14 +1,34 @@
 import random
+import pygame
+import os.path
+from pygame.locals import (
+    RLEACCEL)
 
+folder = os.path.abspath(os.path.dirname(__file__))
 
-class Enemy:
+ENEMY_IMAGE_PATH = os.path.join(folder, "alien.png")
+
+class Enemy(pygame.sprite.Sprite):
 
     def __init__(self, speed, size): 
-
+        pygame.sprite.Sprite.__init__(self)
         print("New enemy created")
+
+        self.surf = pygame.image.load(ENEMY_IMAGE_PATH).convert()
+        transparent_color = self.surf.get_at((0,0))
+        self.surf.set_colorkey(transparent_color, RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center=(100,100))
+            
 
         self.speed = speed
         self.size = size
+
+    def update(self):
+        #print("Hello")
+        self.rect.move_ip(self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
 
     def __str__(self):
         text = ""
@@ -22,3 +42,5 @@ class Enemy:
 if __name__ == "__main__":
     enemy = Enemy(speed=4, size=3)
     print(enemy)
+
+    
--- New Game.py
+++ New Game.py
@@ -1,26 +1,53 @@
 import pygame
 import random
+from pygame.locals import (
+K_ESCAPE,
+KEYDOWN
+)
 import sys
 import os.path
 
-pygame.init()
-
-
-
 from Enemy_NewGame import Enemy
+
+pygame.init()
+screen = pygame.display.set_mode((1000,700))
+
+
+all_sprites = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
 
 enemy = Enemy(speed=random.randint(1,10), size=random.randint(1,10)) # change parameters for size as it depends on image.
 #print(enemy)
 
-SCREEN_HEIGHT = 200
-SCREEN_WIDTH = 200
+all_sprites.add(enemy)
+enemies.add(enemy)
 
 bg_colour = (40, 200, 143)
 
-while True:
-    pygame.display.set_mode()
-    screen = pygame.display.set_mode((200,200))
-    
+
+running = True
+
+# Setup the clock for a decent framerate
+clock = pygame.time.Clock()
+
+print("Hello")
+while running:
+   
+    for entity in enemies:
+        entity.update()
+
     screen.fill(bg_colour)
+
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
     pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
+    # Ensure program maintains a rate of 30 frames per second
+    clock.tick(30)
 
